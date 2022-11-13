ea15cba2ad54029cc789fdd1c4048b67f56ed871
BrennanCoats <brennan.coats@gmail.com>
2022-11-12 17:19:12 -0700

    it actually works now

--- Project/script.js
+++ Project/script.js
@@ -1,34 +1,38 @@
+function generatePassword(){
+    var characterAmount = document.getElementById('characterbox').value;
+    var includeUppercase = document.getElementById ('uppercase').checked;    
+    var includeNumbers = document.getElementById ('numbers').checked;
+    var includeSymbols = document.getElementById ('symbols').checked;
 
+    var passwordcharacters = randomPassword(characterAmount, includeUppercase, includeNumbers, includeSymbols);
+     
+    document.getElementById('passwordDisplay').value = passwordcharacters.toString().replaceAll(',' , '');
+}
 
-const characterAmountEl = document.getElementById('characterbox') 
-const includeUppercaseEl = document.getElementById ('uppercase')    
-const includeNumbersEl = document.getElementById ('numbers')
-const includeSymbolsEl = document.getElementById ('symbols')
-const generateEl = document.getElementById('generate')
-const password = document.getElementById('passwordDisplay')
-
-generateEl.addEventListener('click', generatePassword);
-
-function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols){
-    const characterAmount = characterAmountEl.value
-    const includeUppercase = includeUppercaseEl.checked
-    const includeSymbols = includeSymbolsEl.checked
-    const includeNumbers = includeNumbersEl.checked
-    passwordDisplay.innertext = password
-    var LowercaseCodes = 'abcdefghifklmnopqrstuvwxyz'
-    var UppercaseCodes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-    var numberCodes = '1234567890'
-    var symbolCodes = '!@#$%^&*()'
+function randomPassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+    const LowercaseCodes = 'abcdefghifklmnopqrstuvwxyz'
+    const UppercaseCodes = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const numberCodes = '1234567890'
+    const symbolCodes = '!@#$%^&*()'
     
     let randopass = LowercaseCodes
-    if (includeUppercase) randopass = randopass.concat(UppercaseCodes)
-    if (includeNumbers) randopass = randopass.concat(numberCodes)
-    if (includeSpecialcharacters) randopass = randopass.concat(symbolCodes)
-    const passwordcharacters = []
+    
+    if (includeUppercase) 
+        randopass = randopass.concat(UppercaseCodes);
+    if (includeNumbers) 
+        randopass = randopass.concat(numberCodes);
+    if (includeSymbols) 
+        randopass = randopass.concat(symbolCodes);
+   
+    var passwordcharacters = [];
+    var character = "";
+
     for (let i=0; i < characterAmount; i++) {
-        const character = randopass[Math.floor(Math.random() * randopass.length)]
-        passwordcharacters.push(String.fromrandopass(character))
+        character = randopass [Math.floor(Math.random() * randopass.length)];
+        passwordcharacters.push(character);
     }
-    return passwordcharacters.join('')
+
+    return passwordcharacters;
 }
+
 
